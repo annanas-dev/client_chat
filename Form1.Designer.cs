ec985e7e8505e8c4a526a04231d18b39b562e04a
annanas-dev <annanasidletskaia@icloud.com>
2024-05-11 02:03:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace client_chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Nickname = new System.Windows.Forms.TextBox();
+            this.textBox_Chat = new System.Windows.Forms.TextBox();
+            this.textBox_Message = new System.Windows.Forms.TextBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_SendMessage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Nickname
+            // 
+            this.textBox_Nickname.Location = new System.Drawing.Point(133, 30);
+            this.textBox_Nickname.Name = "textBox_Nickname";
+            this.textBox_Nickname.Size = new System.Drawing.Size(586, 26);
+            this.textBox_Nickname.TabIndex = 0;
+            // 
+            // textBox_Chat
+            // 
+            this.textBox_Chat.Location = new System.Drawing.Point(22, 200);
+            this.textBox_Chat.Multiline = true;
+            this.textBox_Chat.Name = "textBox_Chat";
+            this.textBox_Chat.Size = new System.Drawing.Size(697, 301);
+            this.textBox_Chat.TabIndex = 1;
+            this.textBox_Chat.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_Chat_KeyDown);
+            // 
+            // textBox_Message
+            // 
+            this.textBox_Message.Location = new System.Drawing.Point(22, 85);
+            this.textBox_Message.Multiline = true;
+            this.textBox_Message.Name = "textBox_Message";
+            this.textBox_Message.Size = new System.Drawing.Size(697, 79);
+            this.textBox_Message.TabIndex = 2;
+            this.textBox_Message.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_Message_KeyDown);
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(765, 30);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(210, 40);
+            this.button_Connect.TabIndex = 3;
+            this.button_Connect.Text = "Подключиться";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_SendMessage
+            // 
+            this.button_SendMessage.Location = new System.Drawing.Point(765, 85);
+            this.button_SendMessage.Name = "button_SendMessage";
+            this.button_SendMessage.Size = new System.Drawing.Size(210, 79);
+            this.button_SendMessage.TabIndex = 4;
+            this.button_SendMessage.Text = "Отправить сообщение";
+            this.button_SendMessage.UseVisualStyleBackColor = true;
+            this.button_SendMessage.Click += new System.EventHandler(this.button_SendMessage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите имя";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 638);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_SendMessage);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.textBox_Message);
+            this.Controls.Add(this.textBox_Chat);
+            this.Controls.Add(this.textBox_Nickname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Nickname;
+        private System.Windows.Forms.TextBox textBox_Chat;
+        private System.Windows.Forms.TextBox textBox_Message;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_SendMessage;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client_chart
+{
+    public partial class Form1 : Form
+    {
+        private delegate void Printer(string data);
+        private delegate void Cleaner();
+        Printer MessagePrinter;
+        Cleaner MessageCleaner;
+        private Socket _serverSocket;
+        private Thread _clientThread;
+        private const string _serverHost = "localhost";
+        private const int _serverPort = 010205;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessagePrinter = new Printer(PrintMessage);
+            MessageCleaner = new Cleaner(ClearMessage);
+            ConnectToServer();
+            _clientThread = new Thread(ListenToServer);
+            _clientThread.IsBackground = true;
+            _clientThread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListenToServer()
+        {
+            while (_serverSocket.Connected)
+            {
+                byte[] buffer = new byte[8196];
+                int bytesRec = _serverSocket.Receive(buffer);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                if (data.Contains("#updatechat"))
+                {
+                    UpdateChatMessages(data);
+                    continue;
+                }
+            }
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(_serverHost);
+                IPAddress ipAddress = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, _serverPort);
+                _serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocket.Connect(ipEndPoint);
+            }
+            catch { PrintMessage("Failed to connect to server"); }
+        }
+
+        private void ClearMessage()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(MessageCleaner);
+                return;
+            }
+            textBox_Chat.Clear();
+        }
+
+        private void UpdateChatMessages(string data)
+        {
+            ClearMessage();
+            string[] messages = data.Split('&')[1].Split('|');
+            int countMessages = messages.Length;
+            if (countMessages <= 0) return;
+            for (int i = 0; i < countMessages; i++)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(messages[i])) continue;
+                    PrintMessage(String.Format("[{0}]:{1}.", messages[i].Split('~')[0], messages[i].Split('~')[1]));
+                }
+                catch { continue; }
+            }
+        }
+
+        private void SendMessageToServer(string data)
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                int bytesSent = _serverSocket.Send(buffer);
+            }
+            catch { PrintMessage("Connection with server has been broken"); }
+        }
+
+        private void PrintMessage(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(MessagePrinter, msg);
+                return;
+            }
+            if (textBox_Chat.Text.Length == 0)
+                textBox_Chat.AppendText(msg);
+            else
+                textBox_Chat.AppendText(Environment.NewLine + msg);
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            string Name = textBox_Nickname.Text;
+            if (string.IsNullOrEmpty(Name)) return;
+            SendMessageToServer("#setname&" + Name);
+            textBox_Chat.Enabled = true;
+            textBox_Message.Enabled = true;
+            button_SendMessage.Enabled = true;
+            textBox_Nickname.Enabled = false;
+            button_Connect.Enabled = false;
+        }
+
+        private void SendUserMessage()
+        {
+            try
+            {
+                string data = textBox_Message.Text;
+                if (string.IsNullOrEmpty(data)) return;
+                SendMessageToServer("#newmsg&" + data);
+                textBox_Message.Text = string.Empty;
+            }
+            catch { MessageBox.Show("Error while sending message"); }
+        }
+
+
+        private void button_SendMessage_Click(object sender, EventArgs e)
+        {
+            SendUserMessage();
+        }
+
+        private void textBox_Chat_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendUserMessage();
+        }
+
+        private void textBox_Message_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendUserMessage();
+        }
+    }
+}
